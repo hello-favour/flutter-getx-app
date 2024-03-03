64e9a478cf440a2d03a44f53aa0f227853078192
Favour <hello.williamsfav@gmail.com>
2024-03-02 21:38:11 -0800

    all ui screens done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_app/my_detail_page.dart';
+import 'package:flutter_getx_app/my_home_page.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +12,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: Container(color: Colors.white,),
+      initialRoute: "/",
+      getPages: [
+        GetPage(name: "/", page: () => const MyHomePage()),
+        GetPage(name: "/detail", page: () => const DetailPage()),
+      ],
     );
   }
 }
